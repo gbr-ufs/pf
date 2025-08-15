8cf2f50242a649a41f4155f2e132ee56b90fff86
Gabriel Santos de Souza <gabriel.santosdesouza@dcomp.ufs.br>
2025-08-15 20:38:57 -0300

    feat(code.js): add escolhaDoComputador
    Add a function for determining the CPU's decision.

--- src/code.js
+++ src/code.js
@@ -17,9 +17,26 @@ along with this file.  If not, see <https://www.gnu.org/licenses/>.
 
 // A função aleatorizar gera um número aleatório maior ou igual a zero,
 // porém menor que o número máximo definido,é a função que é usada para
-// determinar a escolha entre pedra,papel ou tesoura, o funcionamento da função é:
+// determinar a escolha entre pedra, papel ou tesoura, o funcionamento da função é:
 // Math.random gera um número entre 0 e 1, esse número é multiplicado pelo número máximo
 // e o Math floor por sua vez corta a parte decimal desse número, o tornando inteiro.
 const aleatorizar = max => {
     return Math.floor(Math.random()*max)
+}
+
+// A função escolhaDoComputador determina, por meio de um número, a escolha que
+// a máquina oponente irá fazer. Como o jogo só possui três opções, um erro é
+// executado caso passe desse número.
+const escolhaDoComputador = numero => {
+    const error = new RangeError("Valor inesperado, deve estar entre 0 e 2")
+
+    if (numero === 0) {
+        return "pedra"
+    } else if (numero === 1) {
+        return "papel"
+    } else if (numero === 2) {
+        return "tesoura"
+    } else {
+        throw error
+    }
 }
