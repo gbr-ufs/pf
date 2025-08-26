f578a26d13704c69fdda313682023793df5457bc
Gabriel Santos de Souza <gabriel.santosdesouza@dcomp.ufs.br>
2025-08-26 19:23:10 -0300

    Remover tudo relacionado a licença
    Segundo o professor, questões de licença não seriam necessárias. Fico
    um pouco triste com isso, pois queria que os projetos fossem software
    livre :(

--- README.md
+++ README.md
@@ -32,7 +32,3 @@ Membros da equipe do projeto.
 - [Maiara Silva Gois](https://github.com/maiarasgois).
 
 ---
-
-# Software Livre
-
-Este programa adere às instruções encontradas em “[Setting Your JavaScript Free](https://www.gnu.org/software/librejs/free-your-javascript.html)” (“Libertando o seu JavaScript”), portanto consta como software livre. Para mais informações sobre JavaScript e Software Livre, leia “[The Free JavaScript campaign](https://www.fsf.org/campaigns/freejs)” (“A campanha Free Javascript”) por [Zak Rogoff](https://www.fsf.org/author/zakkai).
--- src/code.js
+++ src/code.js
@@ -1,21 +1,3 @@
-/**
-SPDX-License-Identifier: GPL-3.0-or-later
-
-This file is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published
-by the Free Software Foundation, either version 3 of the License
-(at your option) any later version.
-
-This file is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this file.  If not, see <https://www.gnu.org/licenses/>.
- */
-
-
 // adiciona um áudio na função de jogar que fica inicia e repetindo ate o jogador reiniciar
 const som = new Audio("src/music.mp3");
 som.loop = true;
